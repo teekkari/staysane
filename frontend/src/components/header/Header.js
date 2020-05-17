03393ba48d4ebc59155c971dc79ff736c4f2e472
Lauri Anttila <lauri.3.anttila@aalto.fi>
2020-05-17 22:22:05 +0300

    Onclick fix for iOS

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -13,14 +13,28 @@ class Header extends React.Component {
         this.menuOpen = false;
     }
 
+    // for some reason iOS didnt register clicks without these present
+    // some stackoverflow magic..
+    componentDidMount() {
+
+        const headerMenuIconContainer = document.getElementById("header-menu-icon");
+
+        headerMenuIconContainer.addEventListener('touchstart', () => {});
+        headerMenuIconContainer.addEventListener('touchend', () => {});
+        //headerMenuIconContainer.addEventListener('touchcancel', () => {});
+        //headerMenuIconContainer.addEventListener('touchmove', () => {});
+    }
+
     toggleHeaderMenu = () => {
 
         if (this.menuOpen) {
             document.getElementById("header").style.height = "100px";
+            document.getElementById("header-menu-icon").style.webkitTransform = "rotate(0deg)";
             document.getElementById("header-menu-icon").style.transform = "rotate(0deg)";
             document.getElementById("header-menu-listgroup").style.opacity = "0";
         } else {
             document.getElementById("header").style.height = "100vh";
+            document.getElementById("header-menu-icon").style.webkitTransform = "rotate(90deg)";
             document.getElementById("header-menu-icon").style.transform = "rotate(90deg)";
             document.getElementById("header-menu-listgroup").style.opacity = "100";
         }
@@ -42,9 +56,9 @@ class Header extends React.Component {
                         <h1>StaySane</h1>
                     </div>
                     <div id="header-menu-icon" onClick={this.toggleHeaderMenu}>
-                    <svg className="bi bi-list" width="40px" height="40px" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" d="M2.5 11.5A.5.5 0 013 11h10a.5.5 0 010 1H3a.5.5 0 01-.5-.5zm0-4A.5.5 0 013 7h10a.5.5 0 010 1H3a.5.5 0 01-.5-.5zm0-4A.5.5 0 013 3h10a.5.5 0 010 1H3a.5.5 0 01-.5-.5z" clipRule="evenodd"/>
-                    </svg>
+                        <svg className="bi bi-list" width="40px" height="40px" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" d="M2.5 11.5A.5.5 0 013 11h10a.5.5 0 010 1H3a.5.5 0 01-.5-.5zm0-4A.5.5 0 013 7h10a.5.5 0 010 1H3a.5.5 0 01-.5-.5zm0-4A.5.5 0 013 3h10a.5.5 0 010 1H3a.5.5 0 01-.5-.5z" clipRule="evenodd"/>
+                        </svg>
                     </div>
                 </Row>
                 <Row id="header-menu-hidden">
