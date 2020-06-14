16844ea2a297549c5572e55acb5d83822c194705
unknown <lanttila98@gmail.com>
2020-06-14 18:20:01 +0300

    Added /settings to Constants.js API consts

--- frontend/src/components/Constants.js
+++ frontend/src/components/Constants.js
@@ -1,9 +1,8 @@
 const API = {
     baseUrl: "http://" + window.location.hostname + ":5000",
-
     authentication: "/users",
-
     modules: "/modules",
+    settings: "/settings"
 };
 
 export default API;
