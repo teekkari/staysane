ff7c08d628d56f1164214d740c31c9484747bc1a
unknown <lanttila98@gmail.com>
2020-08-15 14:43:26 +0300

    stats are now saved with date as iso string and no duplicate stats on single day

--- backend/statistics/statistics.js
+++ backend/statistics/statistics.js
@@ -18,18 +18,30 @@ const statistics = new db.dbCollection('statistics');
 
 function saveDailySnapshot(userID, modules) {
 
+    const date = new Date().toISOString().split('T')[0];
+
     // today's "snapshot", to be appended to data array
     const stat = {
-        date: new Date(),
+        date: date,
         completed: modules.filter( m => m.isDone ).length,
         total: modules.length,
     }
 
     statistics.get({uid: userID }, true).then( (dbResponse) => {
+
+        // no existing data => insert initial
         if (dbResponse === null) {
             statistics.insert({ uid: userID, data: [stat] }).catch( (error) => console.log(error) );
         } else {
-            statistics.update({ uid: userID }, { data: [...dbResponse.data, stat]});
+            
+            const latestDate = dbResponse.data[dbResponse.data.length - 1].date;
+           
+            // if dates dont match, insert data, else replace latest
+            if (date !== latestDate) {
+                statistics.update({ uid: userID }, { data: [...dbResponse.data, stat]});
+            } else {
+                statistics.update({ uid: userID }, { data: [...dbResponse.data.slice(0, -1), stat]});
+            }
         }
     }).catch( (error) => console.log(error) );
 
