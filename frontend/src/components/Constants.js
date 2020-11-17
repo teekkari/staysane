2ed3830fc67eaeea64372d90b327a937eb76b229
unknown <lanttila98@gmail.com>
2020-11-17 14:14:30 +0200

    forgot to save file before last commit

--- frontend/src/components/Constants.js
+++ frontend/src/components/Constants.js
@@ -1,9 +1,5 @@
 const API = {
-<<<<<<< HEAD
-    baseUrl: "http://" + window.location.hostname + ":5050",
-=======
     baseUrl: "http://" + window.location.hostname + ":5100",
->>>>>>> dev
     authentication: "/users",
     modules: "/modules",
     stats: "/stats",
