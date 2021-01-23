5f846013090cd6582d74193cf60f9b6f75edbb8e
unknown <lanttila98@gmail.com>
2021-01-23 15:18:07 +0200

    Added Array.flat support manually due to host machine js not supporting it.

--- backend/authentication.js
+++ backend/authentication.js
@@ -1,6 +1,17 @@
 const db = require('./db/dbHandler.js');
 const usersCollection = new db.dbCollection('users');
 
+
+/* defining flat due to errors on server js support */
+Object.defineProperty(Array.prototype, 'flat', {
+    value: function(depth = 1) {
+      return this.reduce(function (flat, toFlatten) {
+        return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
+      }, []);
+    }
+});
+
+
 // @param sessionKey string hex session key
 // @param resourceIDS string or array of resourceIDs
 // @return promise with resolves to user's document
